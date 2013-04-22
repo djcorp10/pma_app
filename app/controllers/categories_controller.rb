151692599c1239543bcfce840b2362fef5a34e7b
David <djcorp10@smumn.edu>
2013-04-21 23:48:32 -0500

    started user story:11

--- app/controllers/categories_controller.rb
+++
@@ -1,4 +0,0 @@
-class CategoriesController < ApplicationController
-  def new
-  end
-end
--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,4 +1,15 @@
 class QuotesController < ApplicationController
   def new
+  	@quote = Quote.new
+  end
+
+  def create
+    @quote = Quote.new(params[:quote])
+    if @quote.save
+      flash[:success] = "Quote Added."
+      redirect_to new_quote_path
+    else
+      render 'new/quote'
+    end 
   end
 end
