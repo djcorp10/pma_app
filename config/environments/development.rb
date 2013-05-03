7305873593d0d99415552edc31ca3d4ed2d19822
David <djcorp10@smumn.edu>
2013-05-02 21:28:39 -0500

    trying to get mailer to send mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,10 +41,10 @@ config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :domain               => 'baci.lindsaar.net',
-   :user_name            => 'davidcorpstein',
-    :password            => 'secret13',
-   :authentication       => 'login',
+   :domain               => "baci.lindsaar.net",
+   :user_name            => "<username>",
+    :password            => "<password>",
+   :authentication       => "plain",
    :enable_starttls_auto => true
  }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,10 +75,10 @@ config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :domain               => 'baci.lindsaar.net',
-   :user_name            => '<username>',
-    :password            => '<password>',
-   :authentication       => 'login',
+   :domain               => "baci.lindsaar.net",
+   :user_name            => "<username>",
+    :password            => "<password>",
+   :authentication       => "plain",
    :enable_starttls_auto => true
  }
 end
