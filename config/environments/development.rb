6282b788a4ee2739c0760d735002ecb2750d27ec
David <djcorp10@smumn.edu>
2013-05-02 22:31:44 -0500

    still testing the mailer...

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,8 @@ PmaApp::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
@@ -37,11 +38,11 @@ PmaApp::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :domain => "railscasts.com",
-    :user_name => "railscasts",
-    :password => "secret",
-    :authentication => "plain",
-    :enable_starttls_auto => true }
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => 'thedomain.com',
+  :user_name            => 'admin@thedomain.com',
+  :password             => '<password>',
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  }
 end
