23c5c8eefaa81b4aa3005b4ad4d1e7cafea3aa66
David <djcorp10@smumn.edu>
2013-05-02 22:25:50 -0500

    testing mailer

--- config/initializers/setup_mail.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings ={
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => 'thedomain.com',
-  :user_name            => 'admin@thedomain.com',
-  :password             => '<password>',
-  :authentication       => 'plain',
-  :enable_starttls_auto => true
-}
