2d22863c36dddd4b486453b77bcdf768d0edf65b
David <djcorp10@smumn.edu>
2013-05-03 00:27:21 -0500

    fix mailer

--- app/mailers/quote_mailer.rb
+++ app/mailers/quote_mailer.rb
@@ -4,7 +4,7 @@ class QuoteMailer < ActionMailer::Base
   def quote_email(quote)
   	@quote = quote
   	@url = "http://pma-app.herokuapp.com"
-  	mail(:to => quote.email, :subject => "Review your quote!")
+  	# mail(:to => quote.email, :subject => "Review your quote!")
   	mail(:to => "davidcorpstein@gmail.com", :subject => "A new quote has been created!")
   end
 end
